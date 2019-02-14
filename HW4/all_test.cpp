3857f26cfce8022ebe39243712df9b516f3b78f0
tiwarip1 <tiwarip1@msu.edu>
2019-02-13 19:08:29 -0500

    Finished the testing for the randomization function

--- HW4/all_test.cpp
+++ HW4/all_test.cpp
@@ -5,11 +5,23 @@
 using std::cout; using std::endl;
 #include <string>
 using std::string;
+#include <random>
+#include <algorithm>
 
+#include "randompartition.h"
 #include "init_test.h"
+
+void init_testing();
+void rand_testing();
 
 int main(){
 	
+	init_testing();
+	rand_testing();
+	
+}
+
+void init_testing(){
 	cout<<"Testing initialization of the table"<<endl;
 	
 	//Just an initial test to make sure there are no errors
@@ -26,5 +38,29 @@ int main(){
 	//Just a mid range test of the board
 	mid_range_test();
 	
+	cout<<"Everything looks good"<<endl;
+}
+
+void rand_testing(){
+	
+	cout<<"Testing the randomization function"<<endl;
+	
+	int total=100;
+	int regions=10;
+	rarray<int,1> possible_numbers (regions);
+	size_t seed = 1;
+	for(size_t seed=0;seed<regions;seed++){
+		rand_partition(total,regions,possible_numbers,seed);
+		
+		int counter=0;
+		for(size_t i = 0; i<regions;i++){
+			counter+=possible_numbers[i];
+		}
+		
+		if (counter==100){
+			cout<<"Successfully randomized a distribution"<<endl;
+		}
+	}
+	cout<<"Finished Testing randomization function, looks good"<<endl;
 	
 }
