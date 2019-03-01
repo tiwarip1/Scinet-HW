20e311f82f6709468d276d40c36bc7fdc8e9505e
Pranjal Tiwari <herpitehderpo@gmail.com>
2019-03-01 15:17:50 -0500

    have a working code now, which gives reasonable results

--- HW5/jacobian.cpp
+++ HW5/jacobian.cpp
@@ -15,13 +15,19 @@ jacobian (double t, const double y[], double *dfdy,
   float E = 0.015;
   float A = 0.03;
   float C = 0.01;
-  gsl_matrix_view dfdy_mat
-    = gsl_matrix_view_array (dfdy, 2, 2);
+  gsl_matrix_view dfdy_mat = gsl_matrix_view_array (dfdy, 3, 3);
   gsl_matrix * m = &dfdy_mat.matrix;
   gsl_matrix_set (m, 0, 0, -B*y[2]-E*y[1]);
   gsl_matrix_set (m, 0, 1, -E*y[0]);
   gsl_matrix_set (m, 0, 2, -B*y[0]);
+  gsl_matrix_set (m, 1, 0, E*y[1]);
+  gsl_matrix_set (m, 1, 1, -C*y[2]+E*y[0]);
+  gsl_matrix_set (m, 1, 2, -C*y[1]);
+  gsl_matrix_set (m, 2, 0, B*y[2]);
+  gsl_matrix_set (m, 2, 1, C*y[2]-A*y[2]);
+  gsl_matrix_set (m, 2, 2, B*y[0]+C*y[1]-A*y[1]);
   dfdt[0] = 0.0;
   dfdt[1] = 0.0;
+  dfdt[2]=0.0;
   return GSL_SUCCESS;
 }
--- HW5/main.cpp
+++ HW5/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <gsl/gsl_odeiv2.h>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
@@ -6,29 +7,34 @@
 #include "function.h"
 #include "jacobian.h"
 
-int main(){
+using namespace std;
+
+int calculation(int Z0){
   //Defining all the static variables
   float B = 0.02;
   float E = 0.015;
   float A = 0.03;
   float C = 0.01;
   int K0 = 9;
-  int Z0 = 5;
   int S0 = 500-K0-Z0;
   //S=0, K=1, Z=2
-  gsl_odeiv2_system system = {function,jacobian,3,&K0};
+
+  int i;
+  double t=0.0, t1=100.0;
+
+  
+    double param[4] = {.03,.02,.01,.015};
+    double y[3] = { S0, K0 , Z0 };
+
+  gsl_odeiv2_system system = {function,jacobian,3,&param};
 
   gsl_odeiv2_driver * d =
     gsl_odeiv2_driver_alloc_y_new (&system, gsl_odeiv2_step_rk8pd,
-				   1e-6, 1e-6, 0.0);
+                                   1e-6, 1e-6, 0.0);
 
-  int i;
-  double t = 0.0, t1 = 100.0;
-  double y[3] = { S0, K0 , Z0 };
-
-  for (i = 1; i <= 100; i++)
+  for (i = 1; i <= 40; i++)
     {
-      double ti = i * t1 / 100.0;
+      double ti = i * t1 / 40.0;
       int status = gsl_odeiv2_driver_apply (d, &t, ti, y);
 
       if (status != GSL_SUCCESS)
@@ -37,10 +43,21 @@ int main(){
           break;
         }
 
-      printf ("%.5e %.5e %.5e %.5e\n", t, y[0], y[1], y[2]);
+      printf ("%.5e %.5e %.5e %.5e\n", t, round(y[0]), round(y[1]), round(y[2]));
     }
-
+ 
+  cout<<endl;
   gsl_odeiv2_driver_free (d);
+
+  return 0;
+
+}
+
+int main(){
+
+  calculation(7);
+  calculation(8);
+
   return 0;
 
 }
