3c658eb4a43cbfbfe1d983e20cb76f872496c42e
Pranjal Tiwari <herpitehderpo@gmail.com>
2019-04-02 18:23:48 -0400

    finished the job scripts for diffring

--- HW9/parallel.sh
+++ HW9/parallel.sh
@@ -1,15 +1,61 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=16
 #SBATCH --time=1:00:00
-#SBATCH --job-name openmp_job
-#SBATCH --output=openmp_output_%j.txt
+#SBATCH --job-name pranjal16
+#SBATCH --output=diff_16.txt
 #SBATCH --mail-type=FAIL
 
 module load gcc hdf5 netcdf boost gnu-parallel
 
 cd $SLURM_SUBMIT_DIR
 
+export OMP_NUM_THREADS=1
+./diffring
+
+export OMP_NUM_THREADS=2
+./diffring
+
+export OMP_NUM_THREADS=3
+./diffring
+
+export OMP_NUM_THREADS=4
+./diffring
+
+export OMP_NUM_THREADS=5
+./diffring
+
+export OMP_NUM_THREADS=6
+./diffring
+
+export OMP_NUM_THREADS=7
+./diffring
+
+export OMP_NUM_THREADS=8
+./diffring
+
+export OMP_NUM_THREADS=9
+./diffring
+
+export OMP_NUM_THREADS=10
+./diffring
+
+export OMP_NUM_THREADS=11
+./diffring
+
+export OMP_NUM_THREADS=12
+./diffring
+
+export OMP_NUM_THREADS=13
+./diffring
+
+export OMP_NUM_THREADS=14
+./diffring
+
+export OMP_NUM_THREADS=15
+./diffring
+
+export OMP_NUM_THREADS=16
 ./diffring
 
 wait
